9267c0a028aa31cdb51d77d436d6f283fc1200fd
Daniel Machado <danielmcis@gmail.com>
2021-11-25 03:34:04 +0000

    save commit

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -31,6 +31,19 @@ contract Voting is Ownable{
     ProcessStatus public processStatus;
     uint private winningProposalId;
 
+    event VoterRegisteredEvent (address voterAddress); 
+    event ProposalsRegistrationStartedEvent ();
+    event ProposalsRegistrationEndedEvent ();
+    event ProposalRegisteredEvent(uint proposalId);
+    event VotingSessionStartedEvent ();
+    event VotingSessionEndedEvent ();
+    event VotedEvent (address voter, uint proposalId);
+    event VotesTalliedEvent ();
+
+    event WorkflowStatusChangeEvent (
+        ProcessStatus previousStatus, 
+        ProcessStatus newStatus
+    );
     constructor () {
         processStatus = ProcessStatus.RegisteringVoters;
     }
@@ -70,6 +83,8 @@ contract Voting is Ownable{
         voters[_address].isRegistered = true;
         voters[_address].hasVoted = false;
         voters[_address].votedProposalId = 0;
+
+        emit VoterRegisteredEvent(_address);
     }
 
     function removeVoter(address _address) public onlyOwner onlyDuringVotersRegistration isRegistered(_address) {
@@ -80,18 +95,29 @@ contract Voting is Ownable{
 
     function startProposalRegistration() public onlyOwner {
         processStatus = ProcessStatus.ProposalRegistrationStarted;
+
+        emit ProposalsRegistrationStartedEvent ();
+
     }
 
     function addProposal(string memory _name) public isRegisteringProposals isRegistered(msg.sender) {
         proposals.push(Proposal(_name, 0));
+
+        emit ProposalRegisteredEvent(proposals.length - 1 );
+
     }
 
     function endProposalRegistration() public onlyOwner isRegisteringProposals {
         processStatus = ProcessStatus.ProposalRegistrationEnded;
+        emit ProposalsRegistrationEndedEvent ();
+
     }
 
     function startVotesRegistration() public onlyOwner isRegisteringProposalsEnded {
         processStatus = ProcessStatus.VotingStarted;
+
+        emit VotingSessionStartedEvent ();
+
     }
 
     function addVote(uint _proposalId) public isRegisteringVotes isRegistered(msg.sender) {
@@ -100,10 +126,16 @@ contract Voting is Ownable{
         voters[msg.sender].hasVoted = true;
         voters[msg.sender].votedProposalId = _proposalId;
 
+        emit VotedEvent (msg.sender, _proposalId);
+
+
     }
 
     function endVotesRegistration() public onlyOwner isRegisteringVotes {
         processStatus = ProcessStatus.VotingEnded;
+        
+        emit VotingSessionEndedEvent ();
+
     }
 
     function tallyVotes() public onlyOwner isRegisteringVotesEnded {
