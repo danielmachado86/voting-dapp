6ecf37b3ea2b39c42354b7d155b48eb67c7b5743
Daniel Machado <danielmcis@gmail.com>
2021-12-02 22:23:56 +0000

    Save commit

--- src/VotingComponent.js
+++ src/VotingComponent.js
@@ -1,33 +1,46 @@
 import React, {useState} from "react";
+import Voting from '../artifacts/contracts/Voting.sol/Voting.json'
 const { ethers } = require("ethers");
-
 
 
 const VotingComponent = () => {
     
-    const [defaultAccount, setDefaultAccount] = useState(null);
-    
-    const accountChangeHandler = (account) => {
-        setDefaultAccount(account);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  const accountChangeHandler = (account) => {
+    setDefaultAccount(account);
+  }
+  
+  const connectWalletHandler = () => {
+    if(window.ethereum){
+      window.ethereum.request({method: 'eth_requestAccounts'})
+        .then(result => {
+          accountChangeHandler(result[0]);
+          setErrorMessage(null);
+          
+        })
+        .catch(err => setErrorMessage("Conexion rechazada."));
+    } else {
+      setErrorMessage("Por favor instala Metamask!!");
     }
-    
-    const connectWalletHandler = () => {
-      if(window.ethereum){
-        window.ethereum.request({method: 'eth_requestAccounts'})
-          .then(result => {
-            accountChangeHandler(result[0]);
-    
-          })
-      } else {
-        
-      }
+  }
+
+  function ConnectWalletButton() {
+    if(!defaultAccount) {
+      return(<button onClick={connectWalletHandler}>Connect</button>);
     }
+    return(null);
+  }
 
   return (
     <div>
         <h3>{"Voting app"}</h3>
-        <button onClick={connectWalletHandler}>Connect</button>
+        <ConnectWalletButton/>
         <h3>Address: {defaultAccount}</h3>
+
+        {errorMessage}
     </div>
   );
 }
